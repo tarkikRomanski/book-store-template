a20fa6c526e2f85d620d33e4d11a33f7b5759dba
Рома Прокопов < >
2017-04-25 20:17:30 +0300

    All complate

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	/*
+	$(window).resize(function(){
+		if($(window).width() <= 768) {
+			$('#categories_collapse').attr({
+				'data-target': '#categories_list',
+				'data-toggle': 'collapse',
+				'aria-expanded': 'false',
+				'aria-controls': 'categories_list'
+			});
+			$('#categories_list').addClass('collapse');
+		} else {
+			$('#categories_collapse').attr({
+				'data-target': '',
+				'data-toggle': '',
+				'aria-expanded': '',
+				'aria-controls': ''
+			});
+			$('#categories_list').removeClass('collapse');
+		}
+	});
+	*/
+});
